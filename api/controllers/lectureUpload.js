0dad258f2807575ced451bfbabe3063a3903e5cd
kabilza <naruoluvohina@gmail.com>
2022-03-14 02:21:09 +0700

    done: added, install and configured cloudinary

--- api/controllers/lectureUpload.js
+++ api/controllers/lectureUpload.js
@@ -2,14 +2,56 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const uniqid = require("uniqid");
+const cloudinary = require("../../cloud");
 
 const Lecture = require("../models/lecture");
 
-exports.lectureUpload = (req, res, next) => {
-  console.log(req.file);
-  const email = req.body.email
-  res.json({
-    message: "hello",
-    email: email
+exports.lectureUpload = async (req, res, next) => {
+  const {
+    userId,
+    title,
+    author,
+    description,
+    subject,
+    section,
+    professor,
+    numberOfPages,
+    dateCreated,
+    filePath,
+    slug,
+  } = req.body;
+  const { file } = req;
+  const isAlreadyExists = await Lecture.findOne({ slug });
+
+  if (isAlreadyExists) {
+    return res.status(401).json({ error: "Please use unique slug" });
+  }
+
+  const newLecture = new Lecture({
+    userId,
+    title,
+    author,
+    description,
+    subject,
+    section,
+    professor,
+    numberOfPages,
+    dateCreated,
+    filePath,
+    slug,
   });
+
+  if (file) {
+    const { secure_url: url, public_id } = await cloudinary.uploader.upload(
+      file.path
+    );
+    console.log(url);
+    console.log(public_id);
+    console.log('now writing file path to db')
+    newLecture.thumbnail = {url, public_id}
+  }
+
+  await newLecture.save();
+
+  res.json(newLecture);
 };
--- api/models/lecture.js
+++ api/models/lecture.js
@@ -1,66 +1,65 @@
 const mongoose = require("mongoose");
 
-const lectureSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  userId: { type: String, required: true },
-  title: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: true,
-  },
-  subject: {
-    type: String,
-    required: true,
-  },
-  section: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  professor: {
-    type: String,
-    required: true,
-  },
-  numberOfPages: {
-    type: String,
-    required: true,
-  },
-  dateCreated: {
-    type: Date,
-  },
-  meta: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  thumbnail: {
-    type: Object,
-    url: {
-      type: URL,
-      required: true,
-    },
-    public_id: {
+const lectureSchema = mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    title: {
       type: String,
       required: true,
-    }, //storing thumbnails online
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    section: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    professor: {
+      type: String,
+      required: true,
+    },
+    numberOfPages: {
+      type: String,
+      required: true,
+    },
+    dateCreated: {
+      type: Date,
+    },
+    thumbnail: {
+      type: Object,
+      url: {
+        type: URL,
+        required: true,
+      },
+      public_id: {
+        type: String,
+        required: true,
+      }, //storing thumbnails online
+    },
+    filePath: {
+      type: String,
+      required: false,
+    },
+    slug: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
   },
-  fileDestination: {
-    type: String,
-    required: false,
-  },
-  filePath: {
-    type: String,
-    required: false,
-  },
-  slug: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Lecture", lectureSchema);
