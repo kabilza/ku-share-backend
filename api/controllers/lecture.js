c3b4f34fcc0183bbc4504818a171b289c91d6a12
kabilza <naruoluvohina@gmail.com>
2022-03-24 21:09:47 +0700

    pending: doing upload

--- api/controllers/lecture.js
+++ api/controllers/lecture.js
@@ -8,6 +8,10 @@ const bucket = require("../../bucket");
 const Lecture = require("../models/lecture");
 
 // exports.lectureUpload = async (req, res, next) => {
+// console.log(req.body);
+// console.log(req.files.pdf[0].mimetype);
+// console.log('thumbnail file: ')
+// console.log(req.files.thumbnail);
 //   const {
 //     userId,
 //     title,
@@ -58,27 +62,53 @@ const Lecture = require("../models/lecture");
 // };
 
 exports.lectureUpload = async (req, res, next) => {
-  // console.log(req.body);
-  console.log(req.file);
-  const folder = 'lectures'
-  const fileName = `${folder}/${Date.now()}`
+  const reqUserInput = JSON.parse(req.body.userInput);
+
+  const {userId,
+    title,
+    author,
+    description,
+    subject,
+    section,
+    slug} = reqUserInput
+  console.log(description)
+  
+
+  const folder = "lectures";
+  const fileName = `${folder}/${Date.now()}`;
   const fileUpload = bucket.file(fileName);
   const blobStream = fileUpload.createWriteStream({
     metadata: {
-      contentType: req.file.mimetype
-    }
+      contentType: req.files.pdf[0].mimetype,
+    },
   });
 
-  blobStream.on('error', (err) => {
+  blobStream.on("error", (err) => {
     res.status(405).json(err);
   });
 
-  blobStream.on('finish', () => {
-    res.status(200).send('Upload complete!');
+  blobStream.on("finish", () => {
+    const file = bucket.file(fileName);
+    return file
+      .getSignedUrl({
+        action: "read",
+        expires: "03-09-2491",
+      })
+      .then((signedUrls) => {
+        // signedUrls[0] contains the file's public URL
+        console.log("Lecture URL:");
+        console.log(signedUrls[0]);
+        res.status(200).send(signedUrls[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(200).json({
+          err,
+        });
+      });
   });
-
-  blobStream.end(req.file.buffer);
-}
+  blobStream.end(req.files.buffer);
+};
 
 exports.singleLecture = async (req, res, next) => {
   console.log(req);
@@ -86,18 +116,18 @@ exports.singleLecture = async (req, res, next) => {
   const file = bucket.file(`lectures/${req.params.id}`);
 
   // force user to download file when enter the url eg. 'http://localhost:3001/lecture/1648107164740'
-  file.download().then(downloadResponse => {
+  file.download().then((downloadResponse) => {
     res.status(200).send(downloadResponse[0]);
   });
-}
+};
 
 exports.lectureFetching = async (req, res, next) => {
   let myUserId = req.query.userId;
-  console.log('fetch lecture for userId: ')
+  console.log("fetch lecture for userId: ");
   console.log(myUserId);
   const fetchedLectures = await Lecture.find({ userId: myUserId });
   try {
-    return res.status(200).json({myUserId: myUserId});
+    return res.status(200).json({ myUserId: myUserId });
     // return res.status(200).json({fetchedLectures});
   } catch (err) {
     (err) => {
@@ -107,4 +137,4 @@ exports.lectureFetching = async (req, res, next) => {
       });
     };
   }
-}
+};
